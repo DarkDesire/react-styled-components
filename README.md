2630b55188381a479f99c556999bf589bdca1990
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-09-10 16:47:54 +0300

    files updated

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 `npx create-react-app react-styled-components`
 
-`npm install styled-components`
+`npm install styled-components` `npm install @material-ui/core`
 
 `npm start`
 `npm run build`
--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import MyButton from './elements/MyButton';
+import MySection from './elements/MySection';
+import MyHeader from './elements/MyHeader';
+import MaterialButton from './elements/MaterialButton';
+import PageWrapper from './elements/PageWrapper';
+import { GreenTheme, BlueTheme } from './theme/theme';
+import { ThemeProvider } from 'styled-components';
+import { useState } from 'react';
 function App() {
+
+  const pages = [
+    {
+      title: 'page1',
+      buttons: [
+        { text: 'next' }
+      ]
+    },
+
+    {
+      title: 'page2',
+      buttons: [
+        { text: 'prev' },
+        { text: 'next' }
+      ]
+    },
+    {
+      title: 'page3',
+      buttons: [
+        { text: 'prev' },
+        { text: 'next' }
+      ]
+    },
+    {
+      title: 'page4',
+      buttons: [
+        { text: 'prev' }
+
+      ]
+    }
+  ]
+  const [id, setId] = useState(0);
+  let pageDisplay = pages[id];
+  const go = (index) => {
+    if (id == 0 && index == 0) {
+      setId(id + 1)
+    } else if (index == 0) {
+      setId(id - 1)
+    } else {
+      setId(id + 1)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={GreenTheme}>
+      <div>
+        <MyHeader>{pageDisplay.title}</MyHeader>
+        <PageWrapper pid={id} npages={pages.length}>
+          {pageDisplay.buttons.map((button, i) => {
+            return <MyButton key={i} onClick={() => go(i)}>{button.text}</MyButton>
+          })}
+        </PageWrapper>
+      </div>
+    </ThemeProvider>
   );
 }
 
